ff3d21ceb04fb1cb9d071a437272ce4b3c8bd324
Florian Gaune <florian.gaune@gmail.com>
2022-05-05 18:50:49 +0200

    Finalisation des repository

--- src/main/java/com/maplr/testhockeygame/repository/ITeamRepository.java
+++ src/main/java/com/maplr/testhockeygame/repository/ITeamRepository.java
@@ -5,5 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITeamRepository extends JpaRepository<Team, Long> {
 
+    /**
+     * Find a team for a given year
+     * @param year to search
+     * @return a Team of the given year if exists
+     */
     Team findByYear(long year);
 }
